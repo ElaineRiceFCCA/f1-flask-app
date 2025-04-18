16927a354a9509fd2825c0f2e5a418ff356db918
Elaine Rice <elaine.rice@gmail.com>
2025-04-18 21:39:24 +0100

    use port 5000

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def index():
     return render_template('index.html', race=race, season=season, round=round)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 80)
+    app.run(host='0.0.0.0', port = 5000)
